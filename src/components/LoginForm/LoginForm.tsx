531db5407dd31881e228011317c5a345b7d5a45e
sampi74 <143653525+sampi74@users.noreply.github.com>
2023-11-22 10:12:59 -0300

    Add files via upload

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,29 +1,53 @@
-import React from 'react';
 import {MDBBtn, MDBContainer, MDBCard, MDBCardBody, MDBCardImage, MDBRow, MDBCol, MDBInput, MDBCheckbox}
 from 'mdb-react-ui-kit';
 import { useNavigate } from "react-router-dom";
 import { LoginService } from "../../services/LoginService";
 import { DtoLogin } from "../../types/DtoLogin";
 import hamburguesa from '../../assets/images/muzzafoto.jpg'
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import React, { useState } from "react";
+import { toast } from 'react-toastify';
+import { Form, Button, Modal } from "react-bootstrap";
 
 function LoginForm() {
 
   const navigate = useNavigate();
+  const [show, setShow] = useState(true);
 
-    let dtoLogin = {
-        username: "admin",
-        password: "admin",
-    } as DtoLogin;
+    // YUP - Esquema de validación
+    const validationSchema = Yup.object({
+        username: Yup.string().required("El nombre de usuario es requerido"),
+        password: Yup.string().required("La contraseña es requerida"),
+    });
 
-    function onLogIn(){
-        try{
-        LoginService.login(dtoLogin);
-        window.localStorage.setItem('isLoggedIn', 'true');
-        navigate('/')
-        } catch(Error){
-            console.error("Hay un error");
-        }
-    }
+    const handleHide = () => {
+        setShow(false);
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: "",
+        },
+
+        validationSchema: validationSchema,
+
+        onSubmit: async (values) => {
+        try {
+            const token = await LoginService.login(values);
+            console.log("Inicio de sesión exitoso. Token:", token);
+            handleHide();
+            window.localStorage.setItem('isLoggedIn', 'true');
+            navigate('/');
+            toast.success('Inicio de sesión exitoso');
+            } catch (error) {
+            console.error("Error al iniciar sesión:");
+            toast.error('Revisá los datos ingresados');
+            // Puedes mostrar un mensaje de error al usuario o realizar otras acciones según tus necesidades.
+            }
+        },
+    });
 
   return (
     <MDBContainer  className='my-5'>
@@ -37,20 +61,45 @@ function LoginForm() {
 
           <MDBCol md='8'>
 
-            <MDBCardBody>
+          <Form onSubmit={formik.handleSubmit} id="contenedorCamposFormulario">
+                <Form.Group controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        name="username"  
+                        type="text"
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        isInvalid={Boolean(
+                            formik.errors.username && formik.touched.username
+                        )}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.username}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            name="password"
+                            type="password"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            isInvalid={Boolean(
+                                formik.errors.password && formik.touched.password
+                            )} />
+                        <Form.Control.Feedback type="invalid">
+                            {formik.errors.password}
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-              <MDBInput wrapperClass='mb-4' label='Usuario' id='form1' type='string'/>
-              <MDBInput wrapperClass='mb-4' label='Contraseña' id='form2' type='password'/>
-
-              <div className="d-flex justify-content-between mx-4 mb-4">
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Recuerdame' />
-                <a href="!#">Olvidaste tu contraseña?</a>
-              </div>
-
-              <MDBBtn style={{backgroundColor: 'orange', borderColor: 'white'}} onClick={onLogIn} className="mb-4 w-100">Login</MDBBtn>
-
-            </MDBCardBody>
-
+                    <Modal.Footer className="mt-4" id="contenedorBotonRegistrarse">
+                        <Button variant="primary" type="submit" disabled={!formik.isValid} id="botonRegistrarse" >
+                            Iniciar sesion
+                        </Button>
+                    </Modal.Footer>
+                </Form>
           </MDBCol>
 
         </MDBRow>
